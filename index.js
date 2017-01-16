f8b0a1aef562a002f5f099d00e084beabb0eb383
Alan Hortz <alan.hortz@gmail.com>
2017-01-16 15:00:32 +0100

    test

---
+++ index.js
@@ -0,0 +1,43 @@
+/* jshint esversion : 6 */
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const Metawear = require('node-metawear');
+app.use(express.static('public'));
+io.on('connection', function() {
+	console.log('user connected');
+});
+server.listen(8000, function() {
+	console.log('server listening on port 8000');
+});
+
+
+Metawear.discoverByAddress('c7:6e:8e:86:15:55',function(device) {
+    console.log('discovered device ', device.address);
+
+    device.on('disconnect', function() {
+        console.log('we got disconnected! :( ');
+    });
+
+    device.connectAndSetup(function(error) {
+        console.log('were connected!');
+        
+        const QUATERION = 0x7;
+        const DATA_QUATERION = 0x3;
+        const MODE_NDOF = 0x1;
+
+        var sensorFusion = new device.SensorFusion(device);
+        
+        sensorFusion.config.setMode(MODE_NDOF);
+        sensorFusion.subscribe(QUATERION);
+        sensorFusion.enableData(DATA_QUATERION);
+        sensorFusion.start();
+
+        sensorFusion.onChange(function(data) {
+            console.log(data);
+            io.emit('quaternion',data);
+        });
+    });
+});
